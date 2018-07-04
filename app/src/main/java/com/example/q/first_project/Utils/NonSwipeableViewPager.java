9c04217157b2737c62d8e8dd5b59498577096ea2
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-05 00:26:16 +0900

    part 1

---
+++ app/src/main/java/com/example/q/first_project/Utils/NonSwipeableViewPager.java
@@ -0,0 +1,60 @@
+package com.example.q.first_project.Utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
+
+public class NonSwipeableViewPager extends ViewPager{
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    public NonSwipeableViewPager (@NonNull Context context) {
+        super(context);
+        setMyScoller();
+
+    }
+
+    public NonSwipeableViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context);
+        setMyScoller();
+    }
+
+    private void setMyScoller() {
+        try {
+            Class<?> viewPager = ViewPager.class;
+            Field scroller = viewPager.getDeclaredField("myScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new MyScroller(getContext()));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class MyScroller extends Scroller {
+        public MyScroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 400);
+        }
+    }
+}
